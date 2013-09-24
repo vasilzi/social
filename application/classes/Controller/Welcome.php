196772bc34aab52a826b5670bcda343c85b30a87
vasilzi <vasil@grabit.mk>
2013-09-24 14:34:46 +0200

    twitter api test

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -17,7 +17,7 @@ class Controller_Welcome extends Controller_MainController {
 		
 		// Get User ID
 		$user = $facebook->getUser();
-		var_dump($facebook);
+		var_dump($user);
 		$params = array(
 				//'scope' => 'read_stream, friends_likes',
 				'redirect_uri' => 'http://3fstest.si/welcome/facebook'
@@ -25,6 +25,30 @@ class Controller_Welcome extends Controller_MainController {
 		
 		$loginUrl = $facebook->getLoginUrl($params);
 		echo '<a href="'.$loginUrl.'">app</a>';
+		
+		$user_profile=$facebook->api('https://graph.facebook.com/'.$user.'/picture?width=140&height=110');
+		print_r($user_profile);
+	}
+	
+	public function action_twitter()
+	{
+		if (!isset($_GET["oauth_token"])) {
+			$twitter = new TwitterOAuth('pHRwKztbGNnFwtCOcUJFg', '9fkwgwtrcQm1O4SSKaldNbNex9vOePC6k9FdnKTvY');
+			$credentials = $twitter->getRequestToken("http://3fstest.si/welcome/twitter");
+			// append a ?. This is your callback URL if you specify something.
+			$url = $twitter->getAuthorizeUrl($credentials);
+			echo '<a href="'.$url.'">app</a>';
+			Session::instance()->set("token", $credentials["oauth_token"]);
+			Session::instance()->set("secret", $credentials["oauth_token_secret"]);
+		} else {
+			$twitter = new TwitterOAuth('pHRwKztbGNnFwtCOcUJFg', '9fkwgwtrcQm1O4SSKaldNbNex9vOePC6k9FdnKTvY',
+					Session::instance()->get("token"), Session::instance()->get("secret"));
+			$credentials = $twitter->getAccessToken($_GET["oauth_verifier"]);
+			// uses the oauth_token already.
+			// you store these in your database
+			echo '<a href="'.$credentials.'">app</a>';
+		}
 	}
 
 } // End Welcome
+
--- application/logs/2013/09/24.php
+++ application/logs/2013/09/24.php
@@ -172,3 +172,31 @@
 2013-09-24 03:16:05 --- CRITICAL: ErrorException [ 1 ]: Call to a member function set_global() on a non-object ~ APPPATH\classes\Controller\MainController.php [ 26 ] in file:line
 2013-09-24 03:16:05 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
 #1 {main} in file:line
+2013-09-24 07:04:58 --- CRITICAL: ErrorException [ 8 ]: Undefined variable: _SESSION ~ APPPATH\classes\Controller\Welcome.php [ 36 ] in C:\wamp\www\3fs\social\application\classes\Controller\Welcome.php:36
+2013-09-24 07:04:58 --- DEBUG: #0 C:\wamp\www\3fs\social\application\classes\Controller\Welcome.php(36): Kohana_Core::error_handler(8, 'Undefined varia...', 'C:\wamp\www\3fs...', 36, Array)
+#1 C:\wamp\www\3fs\social\system\classes\Kohana\Controller.php(84): Controller_Welcome->action_twitter()
+#2 [internal function]: Kohana_Controller->execute()
+#3 C:\wamp\www\3fs\social\system\classes\Kohana\Request\Client\Internal.php(97): ReflectionMethod->invoke(Object(Controller_Welcome))
+#4 C:\wamp\www\3fs\social\system\classes\Kohana\Request\Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#5 C:\wamp\www\3fs\social\system\classes\Kohana\Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#6 C:\wamp\www\3fs\social\index.php(118): Kohana_Request->execute()
+#7 {main} in C:\wamp\www\3fs\social\application\classes\Controller\Welcome.php:36
+2013-09-24 07:30:00 --- CRITICAL: ErrorException [ 8 ]: Undefined variable: _SESSION ~ APPPATH\classes\Controller\Welcome.php [ 45 ] in C:\wamp\www\3fs\social\application\classes\Controller\Welcome.php:45
+2013-09-24 07:30:00 --- DEBUG: #0 C:\wamp\www\3fs\social\application\classes\Controller\Welcome.php(45): Kohana_Core::error_handler(8, 'Undefined varia...', 'C:\wamp\www\3fs...', 45, Array)
+#1 C:\wamp\www\3fs\social\system\classes\Kohana\Controller.php(84): Controller_Welcome->action_twitter()
+#2 [internal function]: Kohana_Controller->execute()
+#3 C:\wamp\www\3fs\social\system\classes\Kohana\Request\Client\Internal.php(97): ReflectionMethod->invoke(Object(Controller_Welcome))
+#4 C:\wamp\www\3fs\social\system\classes\Kohana\Request\Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#5 C:\wamp\www\3fs\social\system\classes\Kohana\Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#6 C:\wamp\www\3fs\social\index.php(118): Kohana_Request->execute()
+#7 {main} in C:\wamp\www\3fs\social\application\classes\Controller\Welcome.php:45
+2013-09-24 07:32:33 --- CRITICAL: ErrorException [ 8 ]: Undefined index: oauth_token ~ APPPATH\classes\twitteroauth.php [ 114 ] in C:\wamp\www\3fs\social\application\classes\twitteroauth.php:114
+2013-09-24 07:32:33 --- DEBUG: #0 C:\wamp\www\3fs\social\application\classes\twitteroauth.php(114): Kohana_Core::error_handler(8, 'Undefined index...', 'C:\wamp\www\3fs...', 114, Array)
+#1 C:\wamp\www\3fs\social\application\classes\Controller\Welcome.php(46): TwitterOAuth->getAccessToken('oy4KdS54zTg27Jn...')
+#2 C:\wamp\www\3fs\social\system\classes\Kohana\Controller.php(84): Controller_Welcome->action_twitter()
+#3 [internal function]: Kohana_Controller->execute()
+#4 C:\wamp\www\3fs\social\system\classes\Kohana\Request\Client\Internal.php(97): ReflectionMethod->invoke(Object(Controller_Welcome))
+#5 C:\wamp\www\3fs\social\system\classes\Kohana\Request\Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#6 C:\wamp\www\3fs\social\system\classes\Kohana\Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#7 C:\wamp\www\3fs\social\index.php(118): Kohana_Request->execute()
+#8 {main} in C:\wamp\www\3fs\social\application\classes\twitteroauth.php:114
